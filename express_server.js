780a58710ee8b404279ed03849513f94cc6b72cd
vagrant <t.oleary@me.com>
2021-08-03 21:55:59 +0000

    delete is functional on both sides

--- express_server.js
+++ express_server.js
@@ -40,6 +40,14 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL
+
+  delete urlDatabase[shortURL];
+
+  res.redirect('/urls');
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   if (!longURL) {
